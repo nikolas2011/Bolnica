3647899871c4fa4e62fea5952505ee17bc64d122
nikolas2011 <73442861+nikolas2011@users.noreply.github.com>
2022-03-31 02:07:43 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BolnicaVezba
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BolnicaBazaDataContext db = new BolnicaBazaDataContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+            puniGrid();
+            puniOdeljenja();
+            puniPrioritet();
+
+        }
+        void puniPrioritet()
+        {
+            var prioriteti = from p in db.Pacijents
+                             select p;
+
+            cmbPrioritet.ItemsSource = prioriteti;
+        }
+        void puniGrid()
+        {
+            var pacijenti = from p in db.Pacijents
+                            select p;
+
+            dataGridBolnica.ItemsSource = pacijenti;
+        }
+
+        void puniOdeljenja()
+        {
+            var odeljenja = from o in db.Odeljenjes
+                            select o;
+
+            cmbPretragaPoOdeljenju.ItemsSource = odeljenja;
+        }
+        private void cmbPretragaPoOdeljenju_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var pacijenti = from p in db.Pacijents
+                            where p.OdeljenjeID == ((Odeljenje)cmbPretragaPoOdeljenju.SelectedValue).OdeljenjeID
+                            select p;
+
+            lbListaBolesnika.Visibility = Visibility.Visible;
+            lbPacijentiPrioritet.Visibility = Visibility.Hidden;
+
+            lbListaBolesnika.ItemsSource = pacijenti;
+        }
+
+        private void dataGridBolnica_LoadingRowDetails(object sender, DataGridRowDetailsEventArgs e)
+        {
+            naplata();
+        }
+        void naplata()
+        {
+            tbBrDana.Text = (((Pacijent)dataGridBolnica.SelectedValue).BrDana).ToString();
+        }
+
+        private void btnIzracunaj_Click(object sender, RoutedEventArgs e)
+        {
+            int participacija = int.Parse(tbParticipacija.Text);
+            tbNaplata.Text = (participacija * (int.Parse(tbBrDana.Text))).ToString();
+
+            var dan = (from d in db.Pacijents
+                       where d.IDPacijent == ((Pacijent)dataGridBolnica.SelectedValue).IDPacijent
+                       select d).FirstOrDefault();
+             
+            try
+            {
+                db.SubmitChanges();
+                MessageBox.Show("Uspesno ste uplatili" + tbNaplata.Text + "dinara", "Poruka", MessageBoxButton.OK);
+                tbBrDana.Clear();
+                tbParticipacija.Clear();
+                tbNaplata.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Neuspeh!" + ex);
+            }
+
+        }
+
+     
+        private void btnOtpusti_Click(object sender, RoutedEventArgs e)
+        {
+            Pacijent pacijent = (from p in db.Pacijents
+                                 where p.IDPacijent == int.Parse(tbSifraPacijenta.Text)
+                                 select p).First();
+
+            db.Pacijents.DeleteOnSubmit(pacijent);
+
+            try
+            {
+                db.SubmitChanges();
+                MessageBox.Show("Uspesno otpisan pacijent");
+                puniGrid();
+                tbSifraPacijenta.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Neuspeh" + ex);
+            }
+        }
+
+        private void btnMaxDana_Click(object sender, RoutedEventArgs e)
+        {
+            var maxDana = (from p in db.Pacijents
+                           where p.OdeljenjeID == ((Odeljenje)cmbPretragaPoOdeljenju.SelectedValue).OdeljenjeID
+                           select p).Max(p => p.BrDana);
+
+            var selektujPacijenta = from s in db.Pacijents
+                                    where s.BrDana == maxDana
+                                    select s;
+
+            lbListaBolesnika.ItemsSource = selektujPacijenta;
+           
+        }
+
+        private void cmbPrioritet_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var prioritet = (from p in db.Pacijents
+                             join d in db.Odeljenjes
+                             on p.OdeljenjeID equals d.OdeljenjeID
+                             where p.Prioritet == ((Pacijent)cmbPrioritet.SelectedValue).Prioritet
+                             select new { p.Ime, p.Prezime, d.Naziv }
+                             ).ToList();
+
+            lbListaBolesnika.Visibility = Visibility.Hidden;
+            lbPacijentiPrioritet.Visibility = Visibility.Visible;
+
+
+
+            lbPacijentiPrioritet.ItemsSource = prioritet;
+
+        }
+
+        private void UnosNovog_Click(object sender, RoutedEventArgs e)
+        {
+            UnosNovog nov = new UnosNovog();
+            nov.ShowDialog();
+            puniGrid();
+        }
+    }
+        
+}
---
+++ UnosNovog.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BolnicaVezba
+{
+    /// <summary>
+    /// Interaction logic for UnosNovog.xaml
+    /// </summary>
+    public partial class UnosNovog : Window
+    {
+        BolnicaBazaDataContext db = new BolnicaBazaDataContext();
+
+        public UnosNovog()
+        {
+            InitializeComponent();
+            puniOdeljenje();
+           
+        }
+        void puniOdeljenje()
+        {
+            var odeljenje = from o in db.Odeljenjes
+                            select o;
+
+            cmbOdeljenje.ItemsSource = odeljenje;
+
+        }
+
+       
+
+        private void cmbOdeljenje_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var sobe = from s in db.Sobas
+                       where s.OdeljenjeID == ((Odeljenje)cmbOdeljenje.SelectedValue).OdeljenjeID
+                       select s;
+            cmbSoba.ItemsSource = sobe;
+
+        }
+
+        private void cmbSoba_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tbSprat.Text = (((Soba)cmbSoba.SelectedValue).Sprat).ToString();
+        }
+
+        private void btnOdustani_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnUnesi_Click(object sender, RoutedEventArgs e)
+        {
+            Pacijent pacijent = new Pacijent();
+
+            pacijent.IDPacijent = int.Parse(tbSifra.Text);
+            pacijent.Ime = tbIme.Text;
+            pacijent.Prezime = tbPrezime.Text;
+            pacijent.Prioritet = tbPrioritet.Text;
+            pacijent.OdeljenjeID = ((Odeljenje)cmbOdeljenje.SelectedValue).OdeljenjeID;
+            pacijent.SobaID = ((Soba)cmbSoba.SelectedValue).SobaID;
+
+            db.Pacijents.InsertOnSubmit(pacijent);
+
+            try
+            {
+                db.SubmitChanges();
+                MessageBox.Show("Uspesno ste dodali pacijenta");
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Greska" + ex);
+            }
+
+        }
+    }
+}
